88adfbc9b40add011726446a865bd40c08d535c4
nivkopo <nivko8746@gmail.com>
2023-02-11 14:28:34 +0200

    added play, playrandom', print' add song and remove song methods

--- Playlist.cpp
+++ Playlist.cpp
@@ -2,9 +2,78 @@
 
 
 
-void Playlist::Play() {
-	audio_files;
-
-
+Playlist::Playlist(string name) :m_name(name) {
 
 }
+
+// play the songs in alphabetical order
+void Playlist::Play() {
+	set<Song*>::iterator it;
+	for (it = audio_files.begin(); it != audio_files.end(); it++) {
+		m_player.play((*it)->get_path(), true); //todo: check if true is needed (not sure what is the purpose of wait)
+	}
+}
+
+// play the songs randomly
+void Playlist::Play_Random() {
+	// Convert the set to a vector for shuffling
+	std::vector<Song*> audio_files_vector(audio_files.begin(), audio_files.end());
+
+	// Seed the random number generator with the current time
+	std::mt19937 generator(time(nullptr));
+
+	// Shuffle the vector
+	std::shuffle(audio_files_vector.begin(), audio_files_vector.end(), generator);
+
+	// Print the shuffled elements using an iterator
+	std::vector<Song*>::iterator it;
+	for (it = audio_files_vector.begin(); it != audio_files_vector.end(); it++) {
+		m_player.play((*it)->get_path(), true); //todo: check if true is needed (not sure what is the purpose of wait)
+	}
+
+	// Free the dynamically allocated objects
+	for (it = audio_files_vector.begin(); it != audio_files_vector.end(); it++) {
+		delete* it;
+	}
+
+}
+
+
+// print the songs in the playlist (organized alphabetically)
+void Playlist::Print() {
+	set<Song*>::iterator it;
+	int i = 1;
+	cout << "Playlist- " << m_name << ". Songs:";
+	for (it = audio_files.begin(); it != audio_files.end(); it++) {
+		cout << "(" << i << "). " << (*it)->get_name() << endl; 
+		i++;
+	}
+}
+
+// add a song to the playlist. audio files is a multiset
+void Playlist::add_song(Song* song) {
+	audio_files.insert(song);
+}
+
+// remove a song to the playlist. audio files is a multiset
+void Playlist::remove_song(Song* song) {
+	audio_files.erase(song);
+
+}
+
+
+
+
+
+//returns true if the playlists name are in the right order.
+bool operator<(const Playlist& a, const Playlist& b) {
+	return (a.get_name() < b.get_name());
+
+}
+
+
+////returns true if the playlists name are in the right order.
+//bool operator<(Playlist* a, Playlist* b) {
+//	return ((*a).get_name() < (*b).get_name());
+//}
+
--- Playlist.h
+++ Playlist.h
@@ -1,16 +1,38 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+#include <algorithm>
+#include <random>
+#include <ctime>
 #include "AudioCollection.h"
 #include "Song.h"
 
 
 class Playlist : AudioCollection<Song>
 {
+
 private:
 	string m_name;
 	Player m_player;
 
 public:
+	Playlist(string name);
+	void add_song(Song* song);
+	void remove_song(Song* song);
+	string get_name() const { return m_name; }
 	void Play();
-	string get_name() { return m_name; }
+	void Play_Random();
+	void Print();
+
+//friend bool operator<(Playlist* a, Playlist* b);
+
+friend bool operator<(const Playlist& a, const Playlist& b);
+
 
 };
 
+// todo: implement operator < in order set stl will be able to sort
+//bool operator<(Playlist* a, Playlist* b);
+bool operator<(const Playlist& a, const Playlist& b);
+
+
+#endif// PLAYER_H
