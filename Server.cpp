26cee9250e4de46fc67aa20eb424de73e0409aa0
danih <danih@DESKTOP-GCQI5FM>
2023-02-17 13:58:29 +0200

    Wrote upload_song()

--- Server.cpp
+++ Server.cpp
@@ -8,6 +8,27 @@ Server::Server()
     set<int, decltype(compare_by_id)> songs_by_id; //insert here all the songs!
 }
 
+//adds to all data structures
+void Server::upload_song(Song* song)
+{
+    const auto& name = song->get_name(); //avoids unnecessary copies of string objects
+    const auto& album = song->get_album();
+    const auto& artist = song->get_artist();
+    const auto& genre = song->get_genre();
+
+    m_songs_by_alphabet_order.emplace(name, song); //constructs the element directly in place
+    m_all_songs_by_id.emplace(song->get_id(), song);
+    if (!album.empty()) {
+        m_all_songs_by_album.emplace(album, song);
+    }
+    if (!artist.empty()) {
+        m_all_songs_by_artist.emplace(artist, song);
+    }
+    if (!genre.empty()) {
+        m_all_songs_by_genre.emplace(genre, song);
+    }
+}
+
 void Server::upload_podcast_episode(Episode* episode)
 {
     //search for the podcast's name in m_all_podcasts. if exists: add the episode
