038b0bde3f1abf152219c6a4ccbcbfccdd82ca3c
danih <danih@DESKTOP-GCQI5FM>
2023-02-18 14:10:41 +0200

    Episode now has a constructor and it doesn't know its podcast name! Added Play Podcast has a constructor with its name Implemented get and set for its name Added a set of all file paths of episodes to check uniqueness

--- Episode.h
+++ Episode.h
@@ -5,8 +5,14 @@ class Episode :
     public AudioFile
 {
 private:
-    string m_podcast_series_name;
+    //string m_podcast_series_name;
 public:
-    string get_series_name();
+    ////Returns the podcast's name to which the episode belongs to
+    //const string& get_series_name() const;
+    //void set_series_name(const string& podcast_name);
+    //Update the podcast's song
+    Episode(string file_path, string episode_name, int release_date = 0, string duration = "");
+    //Plays the episode
+    void Play();
 };
 
--- Podcast.cpp
+++ Podcast.cpp
@@ -1,12 +1,32 @@
 #include "Podcast.h"
 
+Podcast::Podcast(string podcast_name) : m_podcast_name(podcast_name), m_podcast_episodes() {}
+
+
 void Podcast::Add_Episode(Episode* episode)
 {
-	m_podcast.insert(episode);
+	//if (m_podcast_episodes.empty()) {
+	//	m_podcast_name = episode->get_series_name(); //sets the podcast's name
+	//}
+	m_podcast_episodes.insert(episode);
+	m_episodes_file_paths.insert(episode->get_path());
 }
 
-string& Podcast::Get_Podcast_Name()
+//Chooses the first episode and returns the podcast's name from that
+const string& Podcast::Get_Podcast_Name()
 {
-	auto episode = *(m_podcast.begin());
-	auto name = episode
+	return m_podcast_name;
+}
+bool Podcast::Is_Episode_In_Podcast(const string& file_path)
+{
+	return m_episodes_file_paths.count(file_path) > 0;
+}
+//Updates for each episode the series name
+void Podcast::Set_Podcast_Name(const string& new_name)
+{
+	m_podcast_name = new_name;
+	//for (auto itr = m_podcast_episodes.begin(); itr != m_podcast_episodes.end(); itr++)
+	//{
+	//	(*itr)->set_series_name(new_name);
+	//}
 }
