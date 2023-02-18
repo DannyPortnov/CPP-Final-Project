a48be52f0551e67befe32b0ef0b6d07828f24a30
danih <danih@DESKTOP-GCQI5FM>
2023-02-18 12:08:17 +0200

    Wrote AudioFile, Song and Podcast will inherit from that

--- AudioFile.cpp
+++ AudioFile.cpp
@@ -1,1 +1,23 @@
 #include "AudioFile.h"
+
+int AudioFile::id_code = 1;
+
+AudioFile::AudioFile(string file_name, string file_path, string duration, int release_date)
+	: m_file_name(file_name), m_file_path(file_path), m_duration(duration), m_release_date(release_date)
+{
+	m_id_code = id_code;
+	id_code++;
+}
+
+const string& AudioFile::get_name() const {
+	return m_file_name;
+}
+
+const string& AudioFile::get_path() const {
+	return m_file_path;
+}
+
+// get the id of the song
+const int AudioFile::get_id() const {
+	return m_id_code;
+}
--- AudioFile.h
+++ AudioFile.h
@@ -3,6 +3,7 @@
 #include <set>
 #include <vector>
 #include <string>
+#include "Player.h"
 using namespace std;
 
 //todo: add Podcast class, will inherit from AudioCollection
@@ -18,9 +19,18 @@ protected:
 	int m_release_date;
 	static int id_code;	// in this class we will be able to give id to every music file
 	int m_id_code;
-
+	Player m_player; //todo: add a counter to plays 
+	AudioFile(string file_name, string file_path, string duration="", int release_date = 0);
 public:
+
 	virtual void Play() = 0;
+	virtual void set_duration(string& duration);
+	virtual const string& get_name() const; // get the name of the song
+	virtual const string& get_path() const; // get the name of the song
+	const int get_id() const;
+
+	virtual void set_name(string& name);
+	virtual void set_duration(string& duration);
 };
 
 #endif	// AUDIOFILE_H
