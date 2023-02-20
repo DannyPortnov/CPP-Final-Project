b4efe406181cf2d9bcc89755de89124cd657b775
danih <danih@DESKTOP-GCQI5FM>
2023-02-20 12:06:32 +0200

    Added podcast* to Episode

--- Episode.h
+++ Episode.h
@@ -1,18 +1,21 @@
 #ifndef EPISODE_H
 #define EPISODE_H
 #include "AudioFile.h"
+#include "Podcast.h"
 
 class Episode :
     public AudioFile
 {
 private:
-    //string m_podcast_series_name;
+    //The podcast to which the episode belongs
+    Podcast* m_podcast;
 public:
     ////Returns the podcast's name to which the episode belongs to
     //const string& get_series_name() const;
     //void set_series_name(const string& podcast_name);
     //Update the podcast's song
-    Episode(string file_path, string episode_name, int release_date = 0, string duration = "");
+    Episode(string file_path, string episode_name, Podcast* podcast, int release_date = 0, string duration = "");
+    Podcast* Get_Podcast() const;
     //Plays the episode
     void Play();
 };
