be7d98b015198b3baf1887a49b7e8ef5958e2afc
danih <danih@DESKTOP-GCQI5FM>
2023-02-22 11:18:00 +0200

    Main now calls to library to begin serialization and deserialization

--- main.cpp
+++ main.cpp
@@ -20,34 +20,12 @@ void mp3player() {
 	bool run_program = true;
 	cout << "Welcome To mp3player" << endl;
 	cout << endl;
-	fstream read("c:\\temp\\songs.dat", ios::in);
-	string song_name, artist, album, genre, duration, release_date, file_path;
+	Library::Begin_Serialization();
+	Library::Begin_Deserialization();
 
 	//int id;
-	while (!read.eof()) {
-		read >> song_name >> file_path >> artist >> album >> genre >> duration >> release_date;
-		Library::Add_Song(song_name, file_path, artist, album, genre, duration, release_date);
-		char c1 = read.get(); //Skips the \n at the end of line
-		char c2 = read.peek(); //Peeks at the start of the next line
-		if (c2 == '\n') //if the next line is also \n, quit
-		{
-			break;
-		}
-	}
+	
 	//....
-	fstream write("c:\\temp\\songs.dat", ios::out);
-	unordered_map<int, Song*>::iterator itr;
-	for (itr = Server::get_songs_by_id()->begin(); itr != Server::get_songs_by_id()->end(); itr++)
-	{
-		auto song = itr->second;
-		write << song->get_name() << " " << song->get_path()  << " " << song->get_artist() << " " << song->get_album()
-			<< " " << song->get_genre() << " " << song->get_duration() << " " << song->get_release_date() << endl;
-	}
-
-
-
-
-
 
 }
 
