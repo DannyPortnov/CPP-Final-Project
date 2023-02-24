b7e1c2652da4196954793c5eea3451103c8aedfa
nivkopo <nivko8746@gmail.com>
2023-02-24 16:57:15 +0200

    disabled prints in multiple methods+ added include guards

--- Automatic_Playlist.cpp
+++ Automatic_Playlist.cpp
@@ -13,12 +13,12 @@ Automatic_Playlist::Automatic_Playlist(string name, Library* library) : Playlist
 template <typename TCollection, typename TIter >
 void Automatic_Playlist::Update_Most(TCollection songs_collection, int max_items, TIter iter) {
 	int playlist_size = songs_collection->size();
-	Playlist::clear_all_playlist();
+	Playlist::clear_all_playlist(false); //false for not adding dialog
 	int minimum = min(playlist_size, max_items); // using c++ algorithm
 	// Create an iterator for the list
 	// Traverse through the list using the iterator
 	for (int i = 0; i < minimum; i++) {
-		Playlist::add_song_to_playlist(*iter); // add to recent the updated recently_played linked_list
+		Playlist::add_song_to_playlist(*iter, false); // add to recent the updated data structure. false for not adding dialog
 		iter++;
 	}
 }
